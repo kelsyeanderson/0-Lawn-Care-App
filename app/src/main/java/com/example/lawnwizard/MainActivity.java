39c9e18184615530dc383d84349c0dead5bae59c
lautom36 <lautom36@gmail.com>
2022-02-26 15:49:34 -0700

    added signin and home pages

--- app/src/main/java/com/example/lawnwizard/MainActivity.java
+++ app/src/main/java/com/example/lawnwizard/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.lawnwizard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import com.example.lawnwizard.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // get rid of title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
